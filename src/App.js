b5203e33705f39a54d24f2885d14d943cc51a523
Zak Williams <zak.williams2287@gmail.com>
2019-01-23 00:34:15 -0500

    Completed HeaderNav component layout and styling

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from "react";
+import HeaderNav from "./containers/HeaderNav/HeaderNav";
 
 const API_KEY = "AIzaSyCY1Z702IHnKeFqnF0G6xRxoLnosXwXF2Y";
 
 class App extends Component {
 	render() {
 		return (
-			<div>
-            Aww yee!  Aww yee!!!
-         </div>
+			<HeaderNav />
 		);
 	}
 }
